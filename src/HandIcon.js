ff423e527b9dd111756b3a6cedda37d8ff3c1e9c
candymask0712 <candymask0712@gmail.com>
2024-05-21 10:16:26 +0900

    fix: 로컬 실행 시 발생하는 에러 수정

--- src/HandIcon.js
+++ src/HandIcon.js
@@ -2,18 +2,21 @@ import rockImg from './assets/rock.svg';
 import scissorImg from './assets/scissor.svg';
 import paperImg from './assets/paper.svg';
 import './HandIcon.css';
-import classNames from 'classnames';
 
 const HAND_IMAGES = {
   rock: rockImg,
   scissor: scissorImg,
-  paper: paperImg,
-}
+  paper: paperImg
+};
 
-function HandIcon({ value, className, gameHistory }) { // className prop을 추가하고,
+function HandIcon({ value, className, gameHistory }) {
+  // className prop을 추가하고,
   const src = HAND_IMAGES[value];
-  const winner = gameHistory && gameHistory.length > 0 && gameHistory[gameHistory.length - 1] === '나';
-  const classNames = `${className} ${winner ? 'winner' : ''}`
+  const winner =
+    gameHistory &&
+    gameHistory.length > 0 &&
+    gameHistory[gameHistory.length - 1] === '나';
+  const classNames = `${className} ${winner ? 'winner' : ''}`;
   return <img src={src} alt={value} className={classNames} />; // 이 prop을 img 태그에 적용합니다.
 }
 
