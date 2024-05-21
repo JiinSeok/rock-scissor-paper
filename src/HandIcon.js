4ad690dac386c8f64f24a15287b4c9fe6af17d20
JiinSeok <seokjiin1073@gmail.com>
2024-05-21 11:36:42 +0900

    fix: 승자 css가 적용되지 않는 오류 수정

--- src/HandIcon.js
+++ src/HandIcon.js
@@ -2,19 +2,16 @@ import rockImg from './assets/rock.svg';
 import scissorImg from './assets/scissor.svg';
 import paperImg from './assets/paper.svg';
 import './HandIcon.css';
-import classNames from 'classnames';
 
 const HAND_IMAGES = {
   rock: rockImg,
   scissor: scissorImg,
   paper: paperImg,
-}
+};
 
-function HandIcon({ value, className, gameHistory }) { // className prop을 추가하고,
+function HandIcon({ value, className }) { // className prop을 추가하고,
   const src = HAND_IMAGES[value];
-  const winner = gameHistory && gameHistory.length > 0 && gameHistory[gameHistory.length - 1] === '나';
-  const classNames = `${className} ${winner ? 'winner' : ''}`
-  return <img src={src} alt={value} className={classNames} />; // 이 prop을 img 태그에 적용합니다.
+  return <img src={src} alt={value} className={className} />; // 이 prop을 img 태그에 적용합니다.
 }
 
 export default HandIcon;
